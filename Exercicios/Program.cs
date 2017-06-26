373a70a8415775261f58c42df16894c9b0834730
WashingtonSouza <thewashington.souza@gmail.com>
2017-06-26 19:28:33 -0300

    .

---
+++ Exercicios/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntecessorEMultiplo
+{
+    class Program
+    {
+        static void Main(string[] args)   
+        {
+            Console.WriteLine("Digite um valor ");
+            var a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor ");
+            var b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("O antecessor de {0} é {1} ", a, a -1);             
+            Console.WriteLine("\n");
+            Console.WriteLine("O antecessor de {0} é {1} ", b, b - 1);
+            Console.WriteLine("\n");
+
+            if ((a / b) == 0)
+            {
+                Console.WriteLine(a + " é multiplo de " + b );
+            }
+            else
+                Console.WriteLine(a + " Não é multiplo de " + b);
+
+            Console.ReadKey();
+        }
+    }
+}
